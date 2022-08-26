02a3dd5cbf0e4747d7ee90cf6e01ad2c4f7db5bc
pdpcampos <pedro_caampos@hotmail.com>
2022-08-26 20:41:50 -0300

    primeira informação

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace combustivel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcular o combustivel mais em conta para utilizar no veículo");
+        }
+    }
+}
