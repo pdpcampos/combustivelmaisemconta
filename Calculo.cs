7259d22643bcb57bc39be58caa7d329c737fd280
pdpcampos <pedro_caampos@hotmail.com>
2022-08-26 20:45:25 -0300

    Criando classe calcular

---
+++ Calculo.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combustivel
+{
+    class Calclular
+    {
+        private float Combustivel1;
+
+        public float combustivel1
+        {
+            get { return this.Combustivel1; }
+            set { this.Combustivel1 = value; }
+        }
+
+        private float Combustivel2;
+
+        public float combustivel2
+        {
+            get { return Combustivel2; }
+            set { Combustivel2 = value; }
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine("Alcool " + Combustivel1);
+            Console.WriteLine("Gasolina " + Combustivel2);
+            float total = this.Resultado ();
+
+            if (total <= 0.7)
+            {
+                Console.WriteLine("compensa mais colocar Alcool  " + total);
+            }
+
+            else  
+                    {
+                Console.WriteLine("compensa mais colocar Gasolina  " + total);
+            }
+        }
+
+        private float Resultado()
+        {
+            float total = Combustivel1/Combustivel2;
+            return total;
+        }
+    }
+}
